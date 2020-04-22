26b61396e071637bcc0e6c0ad7fea94eeec8e83b
wang1024 <wskqing@qq.com>
2020-04-22 18:18:27 +0800

    'twosum'

--- src/main/java/com/wang1024/TwoSum.java
+++ src/main/java/com/wang1024/TwoSum.java
@@ -1,7 +1,17 @@
 package com.wang1024;
 
 import java.util.HashMap;
+/*
+*
+给定 nums = [2, 7, 11, 15], target = 9
 
+因为 nums[0] + nums[1] = 2 + 7 = 9
+所以返回 [0, 1]
+
+
+ * @author qqg
+ * @return
+ */
 public class TwoSum {
     public static int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
