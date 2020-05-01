b5a1fe102975ab31657adaa3a7e5785a4698c65f
wang1024 <wskqing@qq.com>
2020-05-01 20:36:57 +0800

    '5.1'

--- src/main/java/com/wang1024/MergeTwoSortedLists/MergeTwoSortedLists.java
+++ src/main/java/com/wang1024/MergeTwoSortedLists/MergeTwoSortedLists.java
@@ -1,4 +1,49 @@
 package com.wang1024.MergeTwoSortedLists;
 
+import com.wang1024.twoAdd.ListNode;
+
 public class MergeTwoSortedLists {
+
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode listnode = new ListNode(-1);
+        ListNode temp = listnode;
+
+        while (l2 != null && l1 != null) {
+            if  (l1.val >= l2.val) {
+                temp.next = l2;
+                l2 = l2.next;
+            } else {
+                temp.next = l1;
+                l1 = l1.next;
+            }
+            temp=temp.next;
+
+        }
+        temp.next= l1==null?l2 :l1;
+
+        return listnode.next;
+    }
+
+    public static void main(String[] args) {
+
+        ListNode a = new ListNode(1);
+        a.next = new ListNode(3);
+        a.next.next = new ListNode(5);
+
+        ListNode b = new ListNode(1);
+        b.next = new ListNode(4);
+        b.next.next = new ListNode(5);
+
+        ListNode l = mergeTwoLists(a, b);
+
+        System.out.println(l.val+"=="+l.next.val+"=="+l.next.next.val+"=="+l.next.next.next.val);
+
+
+    }
+
+
+
 }
+
+
+
--- src/main/java/com/wang1024/lengthOfLongestSubstring/lengthOfLongestSubstring.java
+++ src/main/java/com/wang1024/lengthOfLongestSubstring/lengthOfLongestSubstring.java
@@ -1,4 +1,35 @@
 package com.wang1024.lengthOfLongestSubstring;
 
+import java.util.HashSet;
+
 public class lengthOfLongestSubstring {
+
+    public  static int process(String s){
+        HashSet<Character> set = new HashSet<Character>();
+        int n=s.length();
+
+        int ans=0;
+        int lt=-1;
+        for (int i = 0; i < n; i++) {
+            if (i!=0){
+                set.remove(s.charAt(i-1));
+            }
+
+            while(lt+1<n && !set.contains(s.charAt(lt+1))){
+                set.add(s.charAt(lt + 1));
+                lt++;
+
+            }
+            ans =Math.max(ans,set.size());
+
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int n = process("assadd23456789dddasssadsdddbnmls");
+        System.out.println(n);
+
+    }
 }
