42e8368f04b49566b76b5a18d1f7119420444d3a
wang1024 <wskqing@qq.com>
2020-05-14 22:03:56 +0800

    pow

---
+++ src/main/java/com/wang1024/pow/Pow.java
@@ -0,0 +1,21 @@
+package com.wang1024.pow;
+
+public class Pow {
+
+    public static double pow(double x, int n) {
+        if (n == 0) return 1.0;
+
+        double y = pow(x, n / 2);
+
+        return n % 2 == 0 ? y * y : y * y * x;
+
+    }
+
+    public static void main(String[] args) {
+        double x = 4.0;
+        int n = -4;
+
+        System.out.println(n >= 0 ? pow(x, n) : 1.0 / pow(x, n));
+
+    }
+}
