1d35f5ebe82f0fafa99ad284e23c4a7d8006fe22
Karl-WangSK <wskqing@qq.com>
2020-06-13 23:20:47 +0800

    feat:coins [6.12] 动态规划

---
+++ problems/动态规划/coin/Coin.java
@@ -0,0 +1,22 @@
+package com.wang1024.动态规划.coin;
+
+public class Coin {
+    public  static int coin(int n){
+        int[] coins=new int[]{1,5,10,25};
+        int[] dp=new int[n+1];
+
+        dp[0]=1;
+
+        for (int i = 0; i < coins.length; i++) {
+            for (int j = coins[i]; j <= n; j++) {
+                dp[j] = (dp[j] + dp[j - coins[i]])% 1000000007;
+            }
+        }
+        return dp[n];
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(coin(100));
+    }
+}
