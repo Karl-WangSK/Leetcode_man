e42c0ba937a487d4d751b16c8ac9f3a7c6c12246
Karl-WangSK <wskqing@qq.com>
2020-06-13 23:18:20 +0800

    feat:most_water [2020.05]

---
+++ problems/container_with_most_water/Container_with_most_water.java
@@ -0,0 +1,32 @@
+package com.wang1024.container_with_most_water;
+
+
+public class Container_with_most_water {
+
+    public static int maxArea(int[] height){
+        int max_water=0;
+        int start=0;
+        int end=height.length-1;
+
+        while(start<end){
+            if (height[start]<=height[end]){
+                max_water = Math.max(height[start] * (end - start),max_water);
+                while(height[start+1]<=height[start] && start+1 <end) start++;
+                start++;
+            }else{
+                max_water =  Math.max(height[end] * (end - start),max_water);
+                while(height[end-1]<=height[end] && start <end-1) end--;
+                end--;
+            }
+        }
+        return max_water;
+    }
+
+    public static void main(String[] args) {
+        int max_water = maxArea(new int[]{2, 4, 5, 7, 3, 9, 6, 5, 3});
+
+        System.out.println(max_water);
+
+    }
+
+}
