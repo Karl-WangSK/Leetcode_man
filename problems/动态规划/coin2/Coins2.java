45ea9f6baf05736d5cc8bf07265125fdc2300baa
Karl-WangSK <wskqing@qq.com>
2020-06-14 16:19:30 +0800

    feat:coins [2020.6.14] 动态规划

--- problems/动态规划/coin2/Coins2.java
+++ problems/动态规划/coin2/Coins2.java
@@ -1,4 +1,19 @@
 package 动态规划.coin2;
 
 public class Coins2 {
+
+    public static int maxCombination(int amount, int[] coins){
+
+        int[] dp = new int[amount + 1];
+        dp[0]=1;
+
+        for (int i = 0; i < coins.length; i++) {
+            for (int j = coins[i]; j < dp.length; j++) {
+                dp[j]=dp[j]+dp[j-coins[i]];
+
+            }
+        }
+        return  dp[amount];
+
+    }
 }
