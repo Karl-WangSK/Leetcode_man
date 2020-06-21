8db15bbf14261159225f74e0dd4b9110d55da3f3
Karl-WangSK <wskqing@qq.com>
2020-06-21 18:49:02 +0800

    feat:binary_tree_coin [6.21] DFS

--- problems/深度优先/binary_tree_coin/Binary_tree_coin.java
+++ problems/深度优先/binary_tree_coin/Binary_tree_coin.java
@@ -1,4 +1,26 @@
 package 深度优先.binary_tree_coin;
 
 public class Binary_tree_coin {
+     int res=0;
+    public int distributeCoins(TreeNode root) {
+
+        dfs(root);
+        return res;
+    }
+
+     int dfs(TreeNode root){
+        if (root==null) return 0;
+
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+
+        res+=Math.abs(left)+Math.abs(right);
+
+        return root.val+left+right-1;
+
+    }
+
+
+
+
 }
--- problems/深度优先/binary_tree_coin/TreeNode.java
+++ problems/深度优先/binary_tree_coin/TreeNode.java
@@ -1,4 +1,8 @@
 package 深度优先.binary_tree_coin;
 
 public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
 }
