a47f127b9cc123bff4a033ee1a5aad7bccbe1810
Karl-WangSK <wskqing@qq.com>
2020-06-22 23:41:58 +0800

    treenode

--- problems/深度优先/binary_tree_coin/TreeNode.java
+++ problems/深度优先/binary_tree_coin/TreeNode.java
@@ -1,8 +1,8 @@
 package 深度优先.binary_tree_coin;
 
 public class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode(int x) { val = x; }
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int x) { val = x; }
 }
