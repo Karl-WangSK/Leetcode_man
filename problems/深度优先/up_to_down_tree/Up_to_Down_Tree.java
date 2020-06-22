6dc0c50c0284300d147c52a157bc624edadcf2f6
Karl-WangSK <wskqing@qq.com>
2020-06-22 23:41:38 +0800

    feat:up_to_down_tree [6.22]

---
+++ problems/深度优先/up_to_down_tree/Up_to_Down_Tree.java
@@ -0,0 +1,28 @@
+package 深度优先.up_to_down_tree;
+
+import 深度优先.binary_tree_coin.TreeNode;
+
+import java.util.*;
+
+public class Up_to_Down_Tree {
+    public static List<List<Integer>> levelOrder(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        if(root != null) queue.add(root);
+        while(!queue.isEmpty()) {
+            LinkedList<Integer> tmp = new LinkedList<Integer>();
+            for(int i = queue.size(); i > 0; i--) {
+                TreeNode node = queue.poll();
+                if(res.size() % 2 == 0) tmp.addLast(node.val); // 偶数层 -> 队列头部
+                else tmp.addFirst(node.val); // 奇数层 -> 队列尾部
+                if(node.left != null) queue.add(node.left);
+                if(node.right != null) queue.add(node.right);
+            }
+            res.add(tmp);
+        }
+        return res;
+    }
+
+
+
+}
