2921d227c787a40c2ae927f8b76eed9d2b1a7025
Karl-WangSK <wskqing@qq.com>
2020-06-24 23:09:03 +0800

    remove node from end

--- problems/removeNthFromEnd/removeNthFromEnd.java
+++ problems/removeNthFromEnd/removeNthFromEnd.java
@@ -3,13 +3,7 @@ package removeNthFromEnd;
 
 import twoAdd.ListNode;
 
-/*
-* 给定一个链表: 1->2->3->4->5, 和 n = 2.
 
-当删除了倒数第二个节点后，链表变为 1->2->3->5.
- * @author qqg
- * @return
- */
 public class removeNthFromEnd {
 
     public static ListNode findNth(ListNode head, int n) {
