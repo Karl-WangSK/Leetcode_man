af953736e0888c11a94aae6ef5402285b638588d
Karl-WangSK <wskqing@qq.com>
2020-07-01 22:50:32 +0800

    feat:friend_circle [7.1] bfs

--- problems/深度优先/friend_circle/Friend_circle.java
+++ problems/深度优先/friend_circle/Friend_circle.java
@@ -1,5 +1,8 @@
 package 深度优先.friend_circle;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Friend_circle {
 
     static void dfs(int[][] M, int[] visited, int i) {
@@ -25,6 +28,32 @@ public class Friend_circle {
             }
 
         }
+        return count;
+
+    }
+
+    public int findCircleNum2(int[][] M) {
+
+        int[] visited = new int[M.length];
+        int count = 0;
+        Queue<Integer> queue = new LinkedList<Integer>();
+
+        for (int i = 0; i < M.length; i++) {
+            if (visited[i]==0){
+                queue.add(i);
+                while (!queue.isEmpty()){
+                    Integer n = queue.remove();
+                    visited[n]=1;
+                    for (int j = 0; j < M.length; j++) {
+                        if (M[n][j]==1 && visited[j]==0){
+                            queue.add(j);
+                        }
+                    }
+                }
+                count++;
+            }
+        }
+
         return count;
 
     }
