c286ab4cbef287fb8595a4447699b1f7de774f6a
王仕凯 <wangshikai@wangshikaideMacBook-Pro.local>
2021-04-02 15:26:19 +0800

    feat: atomic

--- problems/template/Singleton/Atomic.scala
+++ problems/template/Singleton/Atomic.scala
@@ -11,18 +11,18 @@ class Atomic {
   Atomic.setActiveContext(this, allowMultipleContexts)
 }
 
-object Atomic{
-  private final val CONTEXT_CONSTRUCTOR_LOCK=new Object()
+object Atomic {
+  private final val CONTEXT_CONSTRUCTOR_LOCK = new Object()
 
   //Set this class as atomic
   private val activeContext: AtomicReference[Atomic] = new AtomicReference[Atomic](null)
 
   //Get new Instance or get latest one
-  def getOrElse(): Atomic ={
-    CONTEXT_CONSTRUCTOR_LOCK.synchronized{
-      if (activeContext.get()==null){
+  def getOrElse(): Atomic = {
+    CONTEXT_CONSTRUCTOR_LOCK.synchronized {
+      if (activeContext.get() == null) {
         setActiveContext(new Atomic(), allowMultipleContexts = false)
-      }else{
+      } else {
         println("Using an existing Context; some configuration may not take effect.")
       }
       activeContext.get()
@@ -30,30 +30,32 @@ object Atomic{
   }
 
   /**
-    * Set active Context if this class is the first time to be called
-    * Else throw exception
-    * @param atomic
-    * @param allowMultipleContexts
-    */
-  def setActiveContext(atomic:Atomic,allowMultipleContexts:Boolean): Unit ={
-    CONTEXT_CONSTRUCTOR_LOCK.synchronized{
+   * Set active Context if this class is the first time to be called
+   * Else throw exception
+   *
+   * @param atomic
+   * @param allowMultipleContexts
+   */
+  def setActiveContext(atomic: Atomic, allowMultipleContexts: Boolean): Unit = {
+    CONTEXT_CONSTRUCTOR_LOCK.synchronized {
       assertNoOtherContextIsRunning(atomic, allowMultipleContexts)
       activeContext.set(atomic)
     }
   }
 
   /**
-    * Ensure there is no other Context is running and global unique
-    * @param atomic
-    * @param allowMultipleContexts
-    */
-  def assertNoOtherContextIsRunning(atomic:Atomic,allowMultipleContexts:Boolean): Unit ={
-    CONTEXT_CONSTRUCTOR_LOCK.synchronized{
-      Option(activeContext.get()).filter(_ ne atomic).foreach{atomic=>
+   * Ensure there is no other Context is running and global unique
+   *
+   * @param atomic
+   * @param allowMultipleContexts
+   */
+  def assertNoOtherContextIsRunning(atomic: Atomic, allowMultipleContexts: Boolean): Unit = {
+    CONTEXT_CONSTRUCTOR_LOCK.synchronized {
+      Option(activeContext.get()).filter(_ ne atomic).foreach { atomic =>
         val exception: Exception = new IllegalStateException("error")
-        if (allowMultipleContexts){
+        if (allowMultipleContexts) {
           println("Multiple running KuduContexts detected in the same JVM!", exception)
-        }else{
+        } else {
           throw new IllegalArgumentException
         }
       }
